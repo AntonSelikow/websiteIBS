c0bee84d961aebb7f7b6fb92363e5c941e299405
Anton Selikow <anton.selikow@googlemail.com>
2015-01-26 12:24:53 +0100

    small jquery changes

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,4 +2,18 @@ $(document).ready(function() {
 	$("#text").click(function() {
 		$("#text").hide("pulsate");
 	});
+	
+	// animate thumbnails
+	$('.thumbnail').mouseenter(function() {
+	       $(this).animate({
+	    	   height: '+=1.5em',
+	    	   width: '+=1.5em'
+	       });
+	   });
+	   $('.thumbnail').mouseleave(function() {
+	       $(this).animate({
+	    	   height: '-=1.5em',
+	    	   width: '-=1.5em'
+	       }); 
+	   });
 });
